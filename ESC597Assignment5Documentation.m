3aaa3d87105cd9a7c3c58feb558ba574ae559609
cxm850 <52715202+cxm850@users.noreply.github.com>
2019-10-02 10:53:26 -0400

    Final code to be compatible with Doxygen

--- ESC597Assignment5Documentation.m
+++ ESC597Assignment5Documentation.m
@@ -1,17 +1,37 @@
 %> @file  ESC597Assingment5Documentation.m
 %> @brief User Manual for ESC 597 Assignment 5
 %======================================================================
-%> @mainpage Documentation for ESC 597 Assignment 5 
+%> @mainpage Documentation for ESC 597 Assignment 5 - Team Rudy
 
 %> @section intro Introduction
 
-%> The @b Assignment5 is an example of using Git and Doxygen. It 
+%> @b Assignment @b 5 is an example of using Git and Doxygen. It 
 %> was created by Chloe Melnick, Federico Krauch, Linying Gao, and
-%Soumyabrata Maiti
+%>Soumyabrata Maiti.
 
 
-%>@section funcDecr Part 3 from Assignment 5 to plot the trajectories. 
-%>Developed by Chloe Melnick  
+%>@section funcDecr Part 3 
+%>This section is used to plot the trajectories of the particles. 
+%>Developed by Chloe Melnick.  
+
+
+
+%>@section funcDecr Part 4 
+%>This section is used to . 
+%>Developed by .  
+
+
+%>@section funcDecr Part 5 
+%>This section is used to . 
+%>Developed by .  
+
+
+%>@section funcDecr Part 6 
+%>This section is used to plot the trajectories of the particles. 
+%>Developed by Chloe Melnick.  
+
+
+
 %>% ======================================================================
 %>%> @brief This function takes the input of A and plots its trajectories. 
 %>%> @param arg1 First argument
